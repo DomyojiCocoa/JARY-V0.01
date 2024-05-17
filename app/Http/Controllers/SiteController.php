e3f5acb06e451eaff6f97a4740161fc7e9c7da1c
Cocoa <felmenyt123@gmail.com>
2024-05-16 20:14:37 -0500

    nose, si la cague me cago en todo

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -119,7 +119,7 @@ class SiteController extends Controller
         $temperature = $data['main']['temp'];
         $weatherDescription = $data['weather'][0]['description'];
 //$weatherDescription
-        $sites = Site::where('weather_preferable', 'LIKE', '%Sol%')->get();
+        $sites = Site::where('weather_preferable', 'LIKE', '%messi%')->get();
 
         return view('recommendation',compact('sites'));
 
